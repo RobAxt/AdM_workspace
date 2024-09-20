72161e4e24e3150e7903d03ae8b6aba0337c4bf0
RobAxt <roberto.axt@gmail.com>
2024-09-19 22:09:27 -0300

    Practica GE2 Ej1

---
+++ Practica GE2/Core/Src/C_functions.c
@@ -0,0 +1,37 @@
+/* ################## Guía de Ejercicios 2 ################## */
+
+#include "functions.h"
+
+/* Ejercicio 1 */
+void pack32to16 (int32_t * vectorIn, int16_t *vectorOut, uint32_t longitud) {
+
+	for (uint8_t i = longitud; i > 0; i --) {
+		vectorOut[i-1] = (int16_t)(vectorIn[i-1]>>16);
+	}
+}
+// 0 1 2
+// 7 1 3
+
+/* Ejercicio 2 */
+uint32_t max (int32_t * vectorIn, uint32_t longitud) {
+	int32_t  maxValor = vectorIn[longitud - 1];
+	uint32_t maxIndice = longitud -1;
+
+	for (uint32_t i = longitud; i > 1; i --) {
+		if(vectorIn[i-2] > maxValor) {
+			maxValor = vectorIn[i-2];
+			maxIndice = i-2;
+		}
+	}
+	return maxIndice;
+}
+
+/* Ejercicio 3 */
+void downSample (int32_t * vectorIn, int32_t * vectorOut, uint32_t longitud, uint32_t N) {
+
+}
+
+/* Ejercicio 4 */
+void invertir (uint16_t * vector, uint32_t longitud) {
+
+}
